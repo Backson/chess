9b80040c49460ce9e246ce4ec19f03c3df2653eb
Lars Thorben Drögemüller <larstd90@gmail.com>
2014-03-05 15:23:59 +0100

    simplified Action comparison operator

--- src/Action.cpp
+++ src/Action.cpp
@@ -5,21 +5,13 @@ bool operator==(const Action &lhs, const Action &rhs) {
 		return false;
 	if (lhs.type != rhs.type)
 		return false;
-	switch (lhs.type) {
-		case MOVE_PIECE:
-		case CAPTURE_PIECE:
-		case CASTLING:
-		case EN_PASSANT:
-			if (lhs.src != rhs.src)
-				return false;
-			if (lhs.dst != rhs.dst)
-				return false;
-			if (lhs.promotion != rhs.promotion)
-				return false;
-			break;
-		default:
-			return true;
-	}
+	if (lhs.src != rhs.src)
+		return false;
+	if (lhs.dst != rhs.dst)
+		return false;
+	if (lhs.promotion != rhs.promotion)
+		return false;
+	
 	return true;
 }
 
