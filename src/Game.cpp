09c3d67e5f0eb8c75d941128b1b5177a0a1159c3
lars <larstd90@gmail.com>
2014-03-28 22:25:51 +0100

    Added font support and game history

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,22 +3,17 @@
 // LIFECYCLE
 
 Game::Game() {
-	const Situation situation;
-	const Tile invalid = Board::INVALID_TILE;
-	Action action = {PLAYER_NONE, DO_NOTHING, invalid, invalid};
-	_history.push_back({situation, action, 0});
+	reset();
 }
 
 Game::Game(const Situation &situation) {
-	const Tile invalid = Board::INVALID_TILE;
-	Action action = {PLAYER_NONE, DO_NOTHING, invalid, invalid};
-	_history.push_back({situation, action, 0});
+	reset(situation);
 }
 
 // ACCESS
 
 const Situation &Game::current_situation() const {
-	return _history.end()->situation;
+	return _history.back().situation;
 }
 
 int Game::position_repetition_counter(const Position &position) const {
@@ -33,13 +28,42 @@ int Game::position_repetition_counter() const {
 	return position_repetition_counter(current_situation());
 }
 
+const std::list<HistoryEntry> &Game::history() const {
+	return _history;
+}
+
 // OPERATION
 
-void Game::action(const Action& a) {
+void Game::action(const Action &a) {
 	Situation situation = current_situation();
 	situation.action(a);
 	// add to history
 	_history.push_back({situation, a, (int)_history.size()});
 	// increment position repetition counter
+	++_position_repetition[situation];
+}
+
+void Game::seek(int number) {
+	int current = _history.back().index;
+	for (int i = current; i > number; --i)
+		pop();
+}
+
+void Game::pop() {
+	--_position_repetition[current_situation()];
+	_history.pop_back();
+}
+
+void Game::reset() {
+	const Situation situation;
+	reset(situation);
+}
+
+void Game::reset(const Situation &situation) {
+	_history.clear();
+	_position_repetition.clear();
+	const Tile invalid(situation.INVALID_TILE);
+	Action action = {PLAYER_NONE, DO_NOTHING, invalid, invalid};
+	_history.push_back(HistoryEntry{situation, action, 0});
 	++_position_repetition[situation];
 }
