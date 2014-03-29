fba14ee7ffbb691a2f176c86d9da2d01fa173f9f
Speedy Consoles <rubihome@gmx.de>
2014-03-29 01:46:38 +0100

    put main in a class

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=g++
 CFLAGS=-c -Wall -std=gnu++0x -g -I"."
-LDFLAGS=-L"." -lallegro -lallegro_primitives -lallegro_image
+LDFLAGS=-L"." -lallegro -lallegro_color -lallegro_primitives -lallegro_image -lallegro_font -lallegro_ttf
 SOURCES=Action.cpp Board.cpp compare.cpp Game.cpp main.cpp Piece.cpp Position.cpp Rules.cpp Situation.cpp vec.cpp View.cpp
 OBJECTS=$(SOURCES:.cpp=.o)
 SRC_FILES=$(addprefix src/,$(SOURCES))
--- src/main.cpp
+++ src/main.cpp
@@ -16,171 +16,177 @@
 static const int TIMER_BPS = 1000;
 static const int64 US_PER_TICK = 1e6 / TIMER_BPS;
 
-struct ProgramContext {
-	ALLEGRO_DISPLAY *display;
-	ALLEGRO_EVENT_QUEUE *event_queue;
-	ALLEGRO_TIMER *timer;
-	ALLEGRO_FONT *font;
+class Main {
+public:
+	void initialize(int argc, char** argv);
+	void finilize();
+	int main(int argc, char** argv);
+private:
+	ALLEGRO_DISPLAY *_display;
+	ALLEGRO_EVENT_QUEUE *_event_queue;
+	ALLEGRO_TIMER *_timer;
+	ALLEGRO_FONT *_font;
 
-	Game *game;
-	Game::HistoryConstIter iter;
-	View *view;
+	Game *_game;
+	Game::HistoryConstIter _iter;
+	View *_view;
 
-	bool panic;
+	bool _panic;
 };
 
-void initialize(int argc, char **argv, ProgramContext *pc)
-{
+int main(int argc, char** argv) {
+	Main main;
+	return main.main(argc, argv);
+}
+
+void Main::initialize(int argc, char **argv) {
 	// init allegro
 	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 	if (!al_install_keyboard()) {
 		fprintf(stderr, "failed to initialize keyboard driver!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 	if (!al_install_mouse()) {
 		fprintf(stderr, "failed to initialize mouse driver!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 	if (!al_init_image_addon()) {
 		fprintf(stderr, "failed to initialize image loading addon!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 	if (!al_init_primitives_addon()) {
 		fprintf(stderr, "failed to initialize primitives drawing addon!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 
 	al_init_font_addon();
 	if (!al_init_ttf_addon()) {
 		fprintf(stderr, "failed to initialize ttf font addon!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 
-	pc->font = al_load_font("Bevan.ttf", -32, ALLEGRO_TTF_MONOCHROME);
-	if (!pc->font) {
-		pc->panic = true;
+	_font = al_load_font("Bevan.ttf", -32, ALLEGRO_TTF_MONOCHROME);
+	if (!_font) {
+		_panic = true;
 		return;
 	}
 
-	pc->timer = al_create_timer(ALLEGRO_BPS_TO_SECS(TIMER_BPS));
-	if (!pc->timer) {
-		pc->panic = true;
+	_timer = al_create_timer(ALLEGRO_BPS_TO_SECS(TIMER_BPS));
+	if (!_timer) {
+		_panic = true;
 		return;
 	}
 
 	if (View::initialize()) {
 		fprintf(stderr, "failed to initialize view!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 
 	// init game model
 	{
 		Situation situation(Position::factoryStandard(), PLAYER_WHITE);
-		pc->game = new Game(situation);
-		const Situation &current = pc->game->current_situation();
+		_game = new Game(situation);
+		const Situation &current = _game->current_situation();
 		auto w = current.width();
 		auto h = current.height();
-		pc->view = new View(w, h, WHITE_AT_THE_BOTTOM, 20);
+		_view = new View(w, h, WHITE_AT_THE_BOTTOM, 20);
 	}
 
 	// init display
 	{
 		al_set_new_display_flags(ALLEGRO_WINDOWED | ALLEGRO_OPENGL);
-		int w = pc->view->getPanelWidthPixels();
-		int h = pc->view->getPanelHeightPixels();
-		pc->display = al_create_display(w + 200, h + 50);
-		if (!pc->display) {
+		int w = _view->getPanelWidthPixels();
+		int h = _view->getPanelHeightPixels();
+		_display = al_create_display(w + 200, h + 50);
+		if (!_display) {
 			fprintf(stderr, "failed to create display!\n");
-			pc->panic = true;
+			_panic = true;
 			return;
 		}
 	}
 
 	// show systom mouse cursor
-	if (!al_show_mouse_cursor(pc->display)) {
+	if (!al_show_mouse_cursor(_display)) {
 		fprintf(stderr, "WARNING: Could not show mouse cursor.\n");
 	}
 
 	// init event queue
-	pc->event_queue = al_create_event_queue();
-	if (!pc->event_queue) {
+	_event_queue = al_create_event_queue();
+	if (!_event_queue) {
 		fprintf(stderr, "failed to create event_queue!\n");
-		pc->panic = true;
+		_panic = true;
 		return;
 	}
 
-	al_register_event_source(pc->event_queue, al_get_keyboard_event_source());
-	al_register_event_source(pc->event_queue, al_get_mouse_event_source());
-	al_register_event_source(pc->event_queue, al_get_display_event_source(pc->display));
+	al_register_event_source(_event_queue, al_get_keyboard_event_source());
+	al_register_event_source(_event_queue, al_get_mouse_event_source());
+	al_register_event_source(_event_queue, al_get_display_event_source(_display));
 }
 
-void finilize(ProgramContext *pc) {
-	if (pc->font) {
-		al_destroy_font(pc->font);
-		pc->font = nullptr;
+void Main::finilize() {
+	if (_font) {
+		al_destroy_font(_font);
+		_font = nullptr;
 	}
-	if (pc->view) {
-		delete pc->view;
-		pc->view = nullptr;
+	if (_view) {
+		delete _view;
+		_view = nullptr;
 	}
-	if (pc->game) {
-		delete pc->game;
-		pc->game = nullptr;
+	if (_game) {
+		delete _game;
+		_game = nullptr;
 	}
 	View::deinitialize();
-	if (pc->timer) {
-		al_destroy_timer(pc->timer);
-		pc->timer = nullptr;
+	if (_timer) {
+		al_destroy_timer(_timer);
+		_timer = nullptr;
 	}
-	if (pc->display) {
-		al_destroy_display(pc->display);
-		pc->display = nullptr;
+	if (_display) {
+		al_destroy_display(_display);
+		_display = nullptr;
 	}
-	if (pc->event_queue) {
-		al_destroy_event_queue(pc->event_queue);
-		pc->event_queue = nullptr;
+	if (_event_queue) {
+		al_destroy_event_queue(_event_queue);
+		_event_queue = nullptr;
 	}
 	al_uninstall_system();
 }
 
-int main(int argc, char** argv)
+int Main::main(int argc, char** argv)
 {
-	//printTypeInformation();
+	_display = nullptr;
+	_event_queue = nullptr;
+	_timer = nullptr;
+	_game = nullptr;
+	_view = nullptr;
+	_font = nullptr;
+	_panic = false;
 
-	ProgramContext pc;
-	pc.display = nullptr;
-	pc.event_queue = nullptr;
-	pc.timer = nullptr;
-	pc.game = nullptr;
-	pc.view = nullptr;
-	pc.font = nullptr;
-	pc.panic = false;
+	initialize(argc, argv);
 
-	initialize(argc, argv, &pc);
-
-	if (pc.panic) {
+	if (_panic) {
 		fprintf(stderr, "Initialization failed.  Terminating program...\n");
-		finilize(&pc);
+		finilize();
 		exit(-1);
 	}
 
 	const Board DEFAULT_BOARD = Board::factoryStandard();
 	const Situation DEFAULT_SITUATION(DEFAULT_BOARD, PLAYER_WHITE);
-	Game &game = *pc.game;
+	Game &game = *_game;
 	game.reset(DEFAULT_SITUATION);
-	pc.iter = --game.history().end();
+	_iter = --game.history().end();
 
-	al_start_timer(pc.timer);
+	al_start_timer(_timer);
 
 	bool shutdown = false;
 	int fps_counter = 0;
@@ -189,11 +195,11 @@ int main(int argc, char** argv)
 	int64 new_us_counter;
 	Tile selection = Tile(-1, -1);
 	while (true) {
-		new_us_counter = US_PER_TICK * al_get_timer_count(pc.timer);
+		new_us_counter = US_PER_TICK * al_get_timer_count(_timer);
 
 		// fetch all events in the queue and process them in order
 		ALLEGRO_EVENT ev;
-		while (!shutdown && al_get_next_event(pc.event_queue, &ev)) {
+		while (!shutdown && al_get_next_event(_event_queue, &ev)) {
 			// select event type
 			switch (ev.type) {
 			case ALLEGRO_EVENT_KEY_DOWN:
@@ -206,17 +212,17 @@ int main(int argc, char** argv)
 					shutdown = true;
 				} else if (key == ALLEGRO_KEY_R) {
 					game.reset(DEFAULT_SITUATION);
-					pc.iter = --game.history().end();
+					_iter = --game.history().end();
 				} else if (key == ALLEGRO_KEY_O) {
-					Orientation orient = pc.view->getOrientation();
+					Orientation orient = _view->getOrientation();
 					orient = (Orientation)(((int)orient + 1) % 4);
-					pc.view->setOrientation(orient);
+					_view->setOrientation(orient);
 				} else if (key == ALLEGRO_KEY_B) {
-					if (pc.iter->index > 0)
-						--pc.iter;
+					if (_iter->index > 0)
+						--_iter;
 				} else if (key == ALLEGRO_KEY_N) {
-					if (pc.iter->index < (int)pc.game->history().size() - 1)
-						++pc.iter;
+					if (_iter->index < (int)game.history().size() - 1)
+						++_iter;
 				}
 				break;
 			}
@@ -226,10 +232,10 @@ int main(int argc, char** argv)
 			case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
 				break;
 			case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
-				const Situation &situation = pc.iter->situation;
+				const Situation &situation = _iter->situation;
 				ALLEGRO_MOUSE_STATE mouse;
 				al_get_mouse_state(&mouse);
-				Tile tile = pc.view->getTileAt(mouse.x, mouse.y);
+				Tile tile = _view->getTileAt(mouse.x, mouse.y);
 				Player active_player = situation.active_player();
 				if (ev.mouse.button == 1) {
 					// click outside the board
@@ -245,14 +251,14 @@ int main(int argc, char** argv)
 					}
 
 					// clicked another self owned piece
-					if (situation[tile].player ==  active_player) {
+					if (situation[tile].player == active_player) {
 						selection = tile;
 						goto NEXT_EVENT;
 					}
 
 					// tried to make a move
 					{
-						game.seek(pc.iter->index);
+						game.seek(_iter->index);
 
 						Rules rules;
 						Action action;
@@ -267,7 +273,7 @@ int main(int argc, char** argv)
 							printf(" (promote to %d)\n", action.promotion);
 							selection = situation.INVALID_TILE;
 
-							pc.iter = --game.history().end();
+							_iter = --game.history().end();
 
 							goto NEXT_EVENT;
 						}
@@ -317,22 +323,22 @@ int main(int argc, char** argv)
 		us_counter = new_us_counter;
 
 		al_set_target_backbuffer(al_get_current_display());
-		pc.view->draw(0.0, 0.0, pc.iter->situation, selection);
+		_view->draw(0.0, 0.0, _iter->situation, selection);
 		auto bg = al_color_name("black");
 		auto fg = al_color_name("white");
 		al_draw_filled_rectangle(440, 0, 640, 490, bg);
 		int lineno = 0;
 		int file = game.current_situation().en_passant_file();
-		al_draw_textf(pc.font, fg, 460, 20 + lineno++ * 32, 0, "en passant:\n");
-		al_draw_textf(pc.font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", file);
-		al_draw_textf(pc.font, fg, 460, 20 + lineno++ * 32, 0, "\n");
-		al_draw_textf(pc.font, fg, 460, 20 + lineno++ * 32, 0, "turn:\n");
-		al_draw_textf(pc.font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", pc.iter->index);
+		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "en passant:\n");
+		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", file);
+		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "\n");
+		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "turn:\n");
+		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", _iter->index);
 		al_flip_display();
 		++fps_counter;
 	} // main loop
 
-	finilize(&pc);
+	finilize();
 
 	// exit program success
 	return 0;
