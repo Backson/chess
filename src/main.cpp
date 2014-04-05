87ef111fd588c89a64a905715e4aaabb339ebe19
lars <larstd90@gmail.com>
2014-04-06 01:42:22 +0200

    refactored main function into more separate functions

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,25 @@ static const int64 US_PER_TICK = 1e6 / TIMER_BPS;
 class Main {
 public:
 	Main() = default;
+	~Main();
 
-	void initialize(int argc, char **argv);
-	void finilize();
-	int main(int argc, char **argv);
+	int run();
 
 private:
+
+	void startSystems();
+	void createWindow();
+
+	void loop();
+	void updateFps();
+	void handleEvents();
+	void handleKeyEvent(int key);
+	void handleClickEvent(int x, int y);
+
+	void makeMove(Tile src, Tile dst);
+
+	void drawFrame();
+
 	ALLEGRO_DISPLAY *_display = nullptr;
 	ALLEGRO_EVENT_QUEUE *_event_queue = nullptr;
 	ALLEGRO_TIMER *_timer = nullptr;
@@ -40,15 +53,75 @@ private:
 	Game::HistoryConstIter _iter;
 	View *_view = nullptr;
 
+	Bot *_bot = nullptr;
+
+	bool _shutdown = false;
+	int _fps_counter = 0;
+	int _fps = 0;
+	int64 _us_counter = 0;
+
+	Tile _selection = Tile(-1, -1);
+
 	bool _panic = nullptr;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 	Main main;
-	return main.main(argc, argv);
+	return main.run();
 }
 
-void Main::initialize(int argc, char **argv) {
+Main::~Main() {
+	if (_bot) {
+		delete _bot;
+		_bot = nullptr;
+	}
+	if (_font) {
+		al_destroy_font(_font);
+		_font = nullptr;
+	}
+	if (_view) {
+		delete _view;
+		_view = nullptr;
+	}
+	if (_game) {
+		delete _game;
+		_game = nullptr;
+	}
+	View::deinitialize();
+	if (_timer) {
+		al_destroy_timer(_timer);
+		_timer = nullptr;
+	}
+	if (_display) {
+		al_destroy_display(_display);
+		_display = nullptr;
+	}
+	if (_event_queue) {
+		al_destroy_event_queue(_event_queue);
+		_event_queue = nullptr;
+	}
+}
+
+int Main::run() {
+	startSystems();
+	createWindow();
+
+	if (_panic) {
+		fprintf(stderr, "Initialization failed.  Terminating program...\n");
+		return -1;
+	}
+
+	_bot = new RandomBot();
+	_bot->reset(_game->current_situation());
+
+	al_start_timer(_timer);
+	loop();
+
+	// exit program success
+	return 0;
+}
+
+void Main::startSystems() {
 	// init allegro
 	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
@@ -75,14 +148,20 @@ void Main::initialize(int argc, char **argv) {
 		_panic = true;
 		return;
 	}
-
-	al_init_font_addon();
+	/*if (!al_init_font_addon()) {
+		fprintf(stderr, "failed to initialize font addon!\n");
+		_panic = true;
+		return;
+	}*/
+	al_init_font_addon(); // returns bool in Allegro 5.1
 	if (!al_init_ttf_addon()) {
-		fprintf(stderr, "failed to initialize ttf font addon!\n");
+		fprintf(stderr, "failed to initialize ttf addon!\n");
 		_panic = true;
 		return;
 	}
+}
 
+void Main::createWindow() {
 	_font = al_load_font("Bevan.ttf", -32, ALLEGRO_TTF_MONOCHROME);
 	if (!_font) {
 		_panic = true;
@@ -107,6 +186,8 @@ void Main::initialize(int argc, char **argv) {
 		Situation situation(move(*shared_board), PLAYER_WHITE);
 		shared_board.reset();
 		_game = new Game(situation);
+		_iter = --_game->history().end();
+
 		const Situation &current = _game->current_situation();
 		auto w = current.width();
 		auto h = current.height();
@@ -126,7 +207,7 @@ void Main::initialize(int argc, char **argv) {
 		}
 	}
 
-	// show systom mouse cursor
+	// show system mouse cursor
 	if (!al_show_mouse_cursor(_display)) {
 		fprintf(stderr, "WARNING: Could not show mouse cursor.\n");
 	}
@@ -144,234 +225,188 @@ void Main::initialize(int argc, char **argv) {
 	al_register_event_source(_event_queue, al_get_display_event_source(_display));
 }
 
-void Main::finilize() {
-	if (_font) {
-		al_destroy_font(_font);
-		_font = nullptr;
+void Main::loop() {
+	while (true) {
+		handleEvents();
+		if (_shutdown)
+			break;
+		updateFps();
+		drawFrame();
 	}
-	if (_view) {
-		delete _view;
-		_view = nullptr;
-	}
-	if (_game) {
-		delete _game;
-		_game = nullptr;
-	}
-	View::deinitialize();
-	if (_timer) {
-		al_destroy_timer(_timer);
-		_timer = nullptr;
-	}
-	if (_display) {
-		al_destroy_display(_display);
-		_display = nullptr;
-	}
-	if (_event_queue) {
-		al_destroy_event_queue(_event_queue);
-		_event_queue = nullptr;
-	}
-	al_uninstall_system();
 }
 
-int Main::main(int argc, char **argv)
-{
-	_display = nullptr;
-	_event_queue = nullptr;
-	_timer = nullptr;
-	_game = nullptr;
-	_view = nullptr;
-	_font = nullptr;
-	_panic = false;
-
-	initialize(argc, argv);
-
-	if (_panic) {
-		fprintf(stderr, "Initialization failed.  Terminating program...\n");
-		finilize();
-		exit(-1);
-	}
-
-	shared_ptr<Board> shared_board = Board::factoryStandard();
-	const Board DEFAULT_BOARD = move(*shared_board);
-	shared_board.reset();
-	const Situation DEFAULT_SITUATION(DEFAULT_BOARD, PLAYER_WHITE);
-	Game &game = *_game;
-	game.reset(DEFAULT_SITUATION);
-	_iter = --game.history().end();
-
-	al_start_timer(_timer);
-
-    RandomBot random_bot;
-	Bot &bot = random_bot;
-	bot.reset(DEFAULT_SITUATION);
-
-	bool shutdown = false;
-	int fps_counter = 0;
-	int fps = 0;
-	int64 us_counter = 0;
-	int64 new_us_counter;
-	Tile selection = Tile(-1, -1);
-	while (true) {
-		new_us_counter = US_PER_TICK * al_get_timer_count(_timer);
-
-		// fetch all events in the queue and process them in order
-		ALLEGRO_EVENT ev;
-		while (!shutdown && al_get_next_event(_event_queue, &ev)) {
-			// select event type
-			switch (ev.type) {
-			case ALLEGRO_EVENT_KEY_DOWN:
-				break;
-			case ALLEGRO_EVENT_KEY_UP:
-				break;
-			case ALLEGRO_EVENT_KEY_CHAR: {
-				auto key = ev.keyboard.keycode;
-				if (key == ALLEGRO_KEY_ESCAPE) {
-					shutdown = true;
-				} else if (key == ALLEGRO_KEY_R) {
-					game.reset(DEFAULT_SITUATION);
-					_iter = --game.history().end();
-				} else if (key == ALLEGRO_KEY_O) {
-					Orientation orient = _view->getOrientation();
-					orient = (Orientation)(((int)orient + 1) % 4);
-					_view->setOrientation(orient);
-				} else if (key == ALLEGRO_KEY_B) {
-					if (_iter->index > 0)
-						--_iter;
-				} else if (key == ALLEGRO_KEY_N) {
-					if (_iter->index < (int)game.history().size() - 1)
-						++_iter;
-				}
-				break;
-			}
-
-			case ALLEGRO_EVENT_MOUSE_AXES:
-				break;
-			case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-				break;
-			case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
-				const Situation &situation = _iter->situation;
-				ALLEGRO_MOUSE_STATE mouse;
-				al_get_mouse_state(&mouse);
-				Tile tile = _view->getTileAt(mouse.x, mouse.y);
-				Player active_player = situation.active_player();
-				if (ev.mouse.button == 1) {
-					// click outside the board
-					if (!situation.isInBound(tile)) {
-						selection = situation.INVALID_TILE;
-						goto NEXT_EVENT;
-					}
-
-					// clicked already selected piece
-					if (tile == selection) {
-						selection = situation.INVALID_TILE;
-						goto NEXT_EVENT;
-					}
-
-					// clicked another self owned piece
-					if (situation[tile].player == active_player) {
-						selection = tile;
-						goto NEXT_EVENT;
-					}
-
-					// tried to make a move
-					{
-						game.seek(_iter->index);
-
-						Rules rules;
-						Action action;
-						action = rules.examineMove(situation, selection, tile);
-						bool is_legal = rules.isActionLegal(situation, action);
-						if (is_legal) {
-							game.action(action);
-							printf("%s: ", action.player == PLAYER_WHITE ? "white" : "black");
-							printf("%c%c -> ", 'A' + action.src[0], '1' + action.src[1]);
-							printf("%c%c", 'A' + action.dst[0], '1' + action.dst[1]);
-							printf(" id:%d", action.type);
-							printf(" (promote to %d)\n", action.promotion);
-							selection = situation.INVALID_TILE;
-
-							bot.update(action);
-							action = bot.next_action();
-
-							if (!rules.isActionLegal(game.current_situation(), action))
-                                printf("bot is retarded.\n");
-
-							game.action(action);
-							printf("%s: ", action.player == PLAYER_WHITE ? "white" : "black");
-							printf("%c%c -> ", 'A' + action.src[0], '1' + action.src[1]);
-							printf("%c%c", 'A' + action.dst[0], '1' + action.dst[1]);
-							printf(" id:%d", action.type);
-							printf(" (promote to %d)\n", action.promotion);
-							selection = situation.INVALID_TILE;
-							bot.update(action);
-
-							_iter = --game.history().end();
-
-							goto NEXT_EVENT;
-						}
-					}
-				}
-				break;
-			}
-
-			case ALLEGRO_EVENT_MOUSE_WARPED:
-				break;
-			case ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY:
-				break;
-			case ALLEGRO_EVENT_MOUSE_LEAVE_DISPLAY:
-				break;
-
-			case ALLEGRO_EVENT_TIMER:
-				break;
-
-			case ALLEGRO_EVENT_DISPLAY_EXPOSE:
-				break;
-			case ALLEGRO_EVENT_DISPLAY_RESIZE:
-				break;
-			case ALLEGRO_EVENT_DISPLAY_CLOSE:
-				shutdown = true;
-				break;
-			case ALLEGRO_EVENT_DISPLAY_LOST:
-			case ALLEGRO_EVENT_DISPLAY_FOUND:
-			case ALLEGRO_EVENT_DISPLAY_SWITCH_OUT:
-			case ALLEGRO_EVENT_DISPLAY_SWITCH_IN:
-			case ALLEGRO_EVENT_DISPLAY_ORIENTATION:
-
-			default:
-				break; // ignore everything we don't know
-			} // select event type
-		NEXT_EVENT:;
-		} // process all events
-
-		if (shutdown)
+void Main::handleEvents() {
+	ALLEGRO_EVENT ev;
+	while (!_shutdown && al_get_next_event(_event_queue, &ev)) {
+		// select event type
+		switch (ev.type) {
+		case ALLEGRO_EVENT_KEY_DOWN:
+			break;
+		case ALLEGRO_EVENT_KEY_UP:
+			break;
+		case ALLEGRO_EVENT_KEY_CHAR:
+			handleKeyEvent(ev.keyboard.keycode);
 			break;
 
-		// whenever a full second has passed
-		if (new_us_counter / 1000000 > us_counter / 1000000) {
-			fps = fps_counter;
-			fps_counter = 0;
+		case ALLEGRO_EVENT_MOUSE_AXES:
+			break;
+		case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+			break;
+		case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+			if (ev.mouse.button == 1)
+				handleClickEvent(ev.mouse.x, ev.mouse.y);
+			break;
+
+		case ALLEGRO_EVENT_MOUSE_WARPED:
+			break;
+		case ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY:
+			break;
+		case ALLEGRO_EVENT_MOUSE_LEAVE_DISPLAY:
+			break;
+
+		case ALLEGRO_EVENT_TIMER:
+			break;
+
+		case ALLEGRO_EVENT_DISPLAY_EXPOSE:
+			break;
+		case ALLEGRO_EVENT_DISPLAY_RESIZE:
+			break;
+		case ALLEGRO_EVENT_DISPLAY_CLOSE:
+			_shutdown = true;
+			break;
+		case ALLEGRO_EVENT_DISPLAY_LOST:
+		case ALLEGRO_EVENT_DISPLAY_FOUND:
+		case ALLEGRO_EVENT_DISPLAY_SWITCH_OUT:
+		case ALLEGRO_EVENT_DISPLAY_SWITCH_IN:
+		case ALLEGRO_EVENT_DISPLAY_ORIENTATION:
+
+		default:
+			break; // ignore everything we don't know
+		} // select event type
+	} // process all events
+}
+
+void Main::updateFps() {
+	int64 new_us_counter = US_PER_TICK * al_get_timer_count(_timer);
+	if (new_us_counter / 1000000 > _us_counter / 1000000) {
+		_fps = _fps_counter;
+		_fps_counter = 0;
+	}
+	_us_counter = new_us_counter;
+}
+
+void Main::handleKeyEvent(int key) {
+	switch (key) {
+		case ALLEGRO_KEY_ESCAPE:
+			_shutdown = true;
+			break;
+
+		case ALLEGRO_KEY_R: {
+			shared_ptr<Board> shared_board = Board::factoryStandard();
+			Situation situation(move(*shared_board), PLAYER_WHITE);
+			shared_board.reset();
+			_game->reset(situation);
+			_iter = --_game->history().end();
+			break;
 		}
-		us_counter = new_us_counter;
 
-		al_set_target_backbuffer(al_get_current_display());
-		_view->draw(0.0, 0.0, _iter->situation, selection);
-		auto bg = al_color_name("black");
-		auto fg = al_color_name("white");
-		al_draw_filled_rectangle(440, 0, 640, 490, bg);
-		int lineno = 0;
-		int file = _iter->situation.en_passant_file();
-		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "fps: %3d\n", fps);
-		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "en passant:\n");
-		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", file);
-		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "\n");
-		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "turn:\n");
-		al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", _iter->index);
-		al_flip_display();
-		++fps_counter;
-	} // main loop
+		case ALLEGRO_KEY_O: {
+			Orientation orient = _view->getOrientation();
+			orient = (Orientation)(((int)orient + 1) % 4);
+			_view->setOrientation(orient);
+			break;
+		}
 
-	finilize();
+		case ALLEGRO_KEY_B:
+			if (_iter->index > 0)
+				--_iter;
+			break;
 
-	// exit program success
-	return 0;
+		case ALLEGRO_KEY_N:
+			if (_iter->index < (int)_game->history().size() - 1)
+				++_iter;
+			break;
+	} // switch
+}
+
+void Main::handleClickEvent(int x, int y) {
+	const Situation &situation = _iter->situation;
+
+	// which tile was clicked?
+	Tile tile = _view->getTileAt(x, y);
+
+	// click outside the board
+	if (!situation.isInBound(tile)) {
+		_selection = situation.INVALID_TILE;
+	}
+
+	// clicked already selected piece
+	else if (tile == _selection) {
+		_selection = situation.INVALID_TILE;
+	}
+
+	// clicked another self owned piece
+	else if (situation[tile].player == situation.active_player()) {
+		_selection = tile;
+	}
+
+	// clicked the second tile to make a move
+	else {
+		makeMove(_selection, tile);
+		_selection = situation.INVALID_TILE;
+	}
+}
+
+void Main::makeMove(Tile src, Tile dst) {
+	const Situation &situation = _iter->situation;
+
+	// forget all moves made after the current one
+	_game->seek(_iter->index);
+
+	Rules rules;
+	Action action = rules.examineMove(situation, src, dst);
+	if (!rules.isActionLegal(situation, action))
+		return;
+
+	_game->action(action);
+	printf("%s: ", action.player == PLAYER_WHITE ? "white" : "black");
+	printf("%c%c -> ", 'A' + action.src[0], '1' + action.src[1]);
+	printf("%c%c", 'A' + action.dst[0], '1' + action.dst[1]);
+	printf(" id:%d", action.type);
+	printf(" (promote to %d)\n", action.promotion);
+
+	_bot->update(action);
+	action = _bot->next_action();
+
+	if (!rules.isActionLegal(_game->current_situation(), action))
+		printf("bot is retarded.\n");
+
+	_game->action(action);
+	printf("%s: ", action.player == PLAYER_WHITE ? "white" : "black");
+	printf("%c%c -> ", 'A' + action.src[0], '1' + action.src[1]);
+	printf("%c%c", 'A' + action.dst[0], '1' + action.dst[1]);
+	printf(" id:%d", action.type);
+	printf(" (promote to %d)\n", action.promotion);
+
+	_bot->update(action);
+
+	_iter = --_game->history().end();
+}
+
+void Main::drawFrame() {
+	al_set_target_backbuffer(al_get_current_display());
+	_view->draw(0.0, 0.0, _iter->situation, _selection);
+	auto bg = al_color_name("black");
+	auto fg = al_color_name("white");
+	al_draw_filled_rectangle(440, 0, 640, 490, bg);
+	int lineno = 0;
+	int file = _iter->situation.en_passant_file();
+	al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "fps: %3d\n", _fps);
+	al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "en passant:\n");
+	al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", file);
+	al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "\n");
+	al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "turn:\n");
+	al_draw_textf(_font, fg, 460, 20 + lineno++ * 32, 0, "%d\n", _iter->index);
+	al_flip_display();
+	++_fps_counter;
 }
